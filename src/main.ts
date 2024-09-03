20b28413fd6e66515964b1f6b19c396ef439997a
Eduardo <eduardolagonunes@gmail.com>
2024-09-03 08:52:55 -0300

    TROCADO PARA PORTA 3K

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
 }
 bootstrap();
